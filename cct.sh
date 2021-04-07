30ab767301c936dd7aad2350872d015a0a3ae4a7
MR M3R4Z <77550679+Ardi12345X@users.noreply.github.com>
2021-04-07 12:20:25 +0700

    Add files via upload

---
+++ cct.sh
@@ -0,0 +1,137 @@
+#script gabut"
+#sv no wa gua +6283192644730"
+
+sleep
+clear
+sleep 3
+echo -e "\e[92mTool gabut :)\e[0m";
+clear 1
+clear
+sleep 0.9
+echo -e "\e[92mNCI=NEW CYBER INDONESIA\e[0m";
+sleep 0.9
+clear
+sleep 0.8
+echo -e "\e[94mhecked by-MR•M3R4Z\e[0m";
+sleep 0.7
+clear
+sleep 1.6
+echo -e "\e[91mTOOL GABUT :-V\e[0m";
+echo -e "\e[93m---------------------------------------------------------------\e[0m";
+sleep 1
+echo -e "\e[91m• Author.        = M.Ardiansyah\e[0m";
+sleep 0.5
+echo -e "\e[92m• no WA.         = +6283192644730\e[0m";
+sleep 0.5
+echo -e "\e[92m• chennel yt.    = MR M3R4Z\e[0m";
+sleep 0.5
+echo -e "\e[92m• Tim.           = NCI NEW CYBER INDONESIA\e[0m";
+sleep 0.8
+echo -e "\e[93m---------------------------------------------------------------\e[0m";
+sleep 1
+echo -e "\e[91m• MTK !\e[0m";
+sleep 2
+echo -e "\e[92m• masukan nama Anda !\e[0m";
+read -p '$>' userinput1;
+#
+sleep 1.9
+echo -e "\e[92m•loading.......\e[0m";
+sleep 2
+echo -e "\e[93m•loading...........\e[0m";
+sleep 1.3
+echo -e "\e[92m•loading......\e[0m";
+sleep 1
+echo -e "\e[93m•loading........\e[0m";
+sleep 1.6
+clear
+sleep 1
+echo -e "\e[92m• ~~~~~~~MTK~~~~~~~• !\e[0m";
+sleep 1.2
+echo -e "\e[92m•kerjakan soal di bawah ini !\e[0m";
+sleep 1
+echo -e "\e[92m• 5 + 5 + 5 × 5 - 5 : 2 = ?\e[0m";
+sleep 0.5
+read -p '$>' userinput2;
+#
+if [ "${userinput2:-}" = "35" ]
+then
+	echo -e "\e[93m• benar \e[0m";
+fi
+if [ "${userinput2:-}" != "35" ]
+then
+	echo -e "\e[91m•salah !
+ jawaban yang benar adalah = 35\e[0m";
+fi
+sleep 2
+clear
+sleep 1
+echo -e "\e[92m• 5 × 5 - 6 × 12 : 1 = ?\e[0m";
+sleep 0.4
+read -p '$>' userinput3;
+#
+if [ "${userinput3:-}" = "228" ]
+then
+	echo -e "\e[93m• benar\e[0m";
+fi
+if [ "${userinput3:-}" != "228" ]
+then
+	echo -e "\e[91m• salah !
+jawaban yang benar adalah = 228\e[0m";
+fi
+sleep 2.5
+clear
+sleep 1
+echo -e "\e[92m• 1 × 1 + 1 = ?\e[0m";
+sleep 0.6
+read -p '$>' userinput4;
+#
+if [ "${userinput4:-}" = "2" ]
+then
+	echo -e "\e[93m• benar\e[0m";
+fi
+if [ "${userinput4:-}" != "2" ]
+then
+	echo -e "\e[91m• salah !
+ jawaban yang benar adalah = 2\e[0m";
+fi
+sleep 2
+clear
+sleep 1.6
+echo -e "\e[92m• 5 × 0 = ?\e[0m";
+sleep 0.6
+read -p '$>' userinput5;
+#
+if [ "${userinput5:-}" = "0" ]
+then
+	echo -e "\e[93m• benar\e[0m";
+fi
+if [ "${userinput5:-}" != "0" ]
+then
+	echo -e "\e[91m• salah !
+ jawaban yang benar adalah = 0\e[0m";
+fi
+sleep 2
+clear
+echo -e "\e[92m• 10 - 10 = ?\e[0m";
+sleep 0.6
+read -p '$>' userinput6;
+#
+if [ "${userinput6:-}" = "0" ]
+then
+	echo -e "\e[33m• benar\e[0m";
+fi
+if [ "${userinput6:-}" != "0" ]
+then
+	echo -e "\e[91m• salah !
+ jawaban yang benar adalah = 0\e[0m";
+fi
+sleep 2
+echo -e "\e[94m• makasih udah pakai tool ini :)\e[0m";
+sleep 1
+echo -e "\e[37m• ketik exit untuk keluar !\e[0m";
+sleep 0.9
+read -p '}~NCI-TIM}>>>~›' userinput7;
+#
+sleep 3
+
+#makasih udah pakai tool ini"
